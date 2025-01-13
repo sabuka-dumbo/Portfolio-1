b0079a23f1bbcd612bd44e13ff336b6a8f95bf15
Sabuka-Dumbo <anonym.sas@proton.me>
2025-01-13 02:34:37 +0000

    Jan 13, 2025, 6:34 AM

--- Portfolio/SabaDumbadze/static/js/index.js
+++ Portfolio/SabaDumbadze/static/js/index.js
@@ -7,25 +7,37 @@ navbar.addEventListener("animationend", function() {
 })
 
 const info_button = document.getElementById("info-button");
+let cooldown = 0;
 
 info_button.addEventListener("mouseenter", function() {
-    info_button.style.animation = "info_button_hover 0.4s ease";
+    if (cooldown == 0) {
+        cooldown = 1;
+        info_button.style.animation = "info_button_hover 0.2s ease";
 
-    info_button.addEventListener("animationend", function() {
-        info_button.style.border = "0px solid black";
-        info_button.style.boxShadow = "8px 8px black";
-        info_button.style.animation = '';
-        info_button.style.marginTop = "9px";
-    })
+        info_button.addEventListener("animationend", function() {
+            info_button.style.border = "0px solid black";
+            info_button.style.boxShadow = "8px 8px black";
+            info_button.style.animation = '';
+            info_button.style.marginTop = "9px";
+
+            cooldown = 0;
+        })
+    }
 })
 
 info_button.addEventListener("mouseleave", function() {
-    info_button.style.animation = "info_button_unhover 0.4s ease";
+    if (cooldown == 0) {
+        cooldown = 1;
 
-    info_button.addEventListener("animationend", function() {
-        info_button.style.border = "5px solid black";
-        info_button.style.boxShadow = '';
-        info_button.style.animation = '';
-        info_button.style.marginTop = '';
-    })
+        info_button.style.animation = "info_button_unhover 0.2s ease";
+
+        info_button.addEventListener("animationend", function() {
+            info_button.style.border = "5px solid black";
+            info_button.style.boxShadow = '';
+            info_button.style.animation = '';
+            info_button.style.marginTop = '';
+
+            cooldown = 0;
+        })
+    }
 })
