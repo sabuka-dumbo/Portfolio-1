66c2c84975844ffc26195b97d06cf93ea76e758d
Sabuka-Dumbo <anonym.sas@proton.me>
2025-01-13 02:31:53 +0000

    Jan 13, 2025, 6:31 AM

--- Portfolio/SabaDumbadze/static/js/index.js
+++ Portfolio/SabaDumbadze/static/js/index.js
@@ -5,3 +5,27 @@ navbar.style.animation = "navbar_animation_open 1.2s ease";
 navbar.addEventListener("animationend", function() {
     navbar.style.animation = '';
 })
+
+const info_button = document.getElementById("info-button");
+
+info_button.addEventListener("mouseenter", function() {
+    info_button.style.animation = "info_button_hover 0.4s ease";
+
+    info_button.addEventListener("animationend", function() {
+        info_button.style.border = "0px solid black";
+        info_button.style.boxShadow = "8px 8px black";
+        info_button.style.animation = '';
+        info_button.style.marginTop = "9px";
+    })
+})
+
+info_button.addEventListener("mouseleave", function() {
+    info_button.style.animation = "info_button_unhover 0.4s ease";
+
+    info_button.addEventListener("animationend", function() {
+        info_button.style.border = "5px solid black";
+        info_button.style.boxShadow = '';
+        info_button.style.animation = '';
+        info_button.style.marginTop = '';
+    })
+})
